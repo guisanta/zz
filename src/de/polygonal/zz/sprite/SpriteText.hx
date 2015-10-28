f67e6b6f6f2621768ed5f190849b4002cdd97c7e
Michael Baczynski <michaelbaczynski@gmail.com>
2015-10-28 09:06:17 +0100

    Fix multiline

--- src/de/polygonal/zz/sprite/SpriteText.hx
+++ src/de/polygonal/zz/sprite/SpriteText.hx
@@ -464,8 +464,8 @@ private class Shaper
 		var scale = settings.size / charSet.renderedSize;
 		var stepY = (charSet.lineHeight + settings.leading) * scale;
 		var numLines = Std.int(boxH / stepY);
-		if (numLines == 0) return false;
-		if (settings.multiline) numLines = 1;
+		if (numLines == 0) return true;
+		if (!settings.multiline) numLines = 1;
 		
 		var codes = mCharCodes, code, lastCode = 0;
 		var kerningAmount = 0;
