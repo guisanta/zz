ff5e125abdf1efd27020da443193ea0b462e9185
Michael Baczynski <michaelbaczynski@gmail.com>
2016-03-31 21:35:53 +0200

    Refactor SpriteTools

--- src/de/polygonal/zz/sprite/SpriteTools.hx
+++ src/de/polygonal/zz/sprite/SpriteTools.hx
@@ -19,11 +19,9 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 package de.polygonal.zz.sprite;
 
 import de.polygonal.core.math.Coord2.Coord2f;
-import de.polygonal.ds.ArrayList;
+import de.polygonal.ds.ArrayedStack;
 import de.polygonal.zz.scene.*;
 import de.polygonal.zz.scene.Spatial.as;
-
-using de.polygonal.ds.tools.NativeArrayTools;
 
 /**
 	Helper methods to operate on hierarchical sprite structures.
@@ -35,93 +33,117 @@ using de.polygonal.ds.tools.NativeArrayTools;
 class SpriteTools
 {
 	static var _aSpatial = new Array<Spatial>();
-	static var _aSpriteBase = new Array<SpriteBase>();
 	static var _tmpCoord = new Coord2f(0, 0);
 	
-	static var _spatialStack = new ArrayList<Spatial>(256);
+	static var _spatialStack = new ArrayedStack<Spatial>();
 	
-	/**
-		Counts the total number of descendants of root.
-	**/
-	public static function countDescendants(root:SpriteGroup):Int
+	public static function gc()
 	{
-		var a = _aSpatial;
-		var top = 1, c = 0, s:Spatial;
-		a[0] = root.mNode;
-		while (top != 0)
-		{
-			s = a[--top];
-			a[top] = null;
-			c++;
-			if (s.isNode())
-			{
-				s = as(s, Node).child;
-				while (s != null)
-				{
-					a[top++] = s;
-					s = s.mSibling;
-				}
-			}
-		}
-		
-		return c - 1;
+		_spatialStack.clear(true);
 	}
 	
-	/**
+ 	/**
 		Creates an iterator over all descendants of root.
 		Uses a non-allocating, iterative traversal.
 	**/
-	public static function descendantsIterator(root:SpriteGroup):Iterator<SpriteBase>
+	public static function descendantsIterator(root:SpriteGroup, ordered:Bool = true):Iterator<SpriteBase>
 	{
-		var a = _aSpatial;
-		var top = 0, n:Node, s:Spatial, k:Int, p:Int, c:Spatial;
-		
-		n = root.mNode;
-		k = n.numChildren;
-		p = top + k;
-		c = n.child;
-		while (c != null)
+		if (ordered)
 		{
-			a[p++] = c;
-			c = c.mSibling;
-		}
-		for (i in 0...k)
-		{
-			a[top++] = a[--p];
-			a[p] = null;
-		}
-		
-		return
-		{
-			hasNext: function()
+			var a = [];
+			var top = 0, n:Node, s:Spatial, k:Int, p:Int, c:Spatial;
+			
+			n = root.mNode;
+			k = n.numChildren;
+			p = top + k;
+			c = n.child;
+			while (c != null)
 			{
-				return top > 0;
-			},
-			next: function()
+				a[p++] = c;
+				c = c.mSibling;
+			}
+			for (i in 0...k)
 			{
-				var s = a[--top];
-				a[top] = null;
-				
-				if (s.isNode())
+				a[top++] = a[--p];
+			}
+			
+			return
+			{
+				hasNext: function()
 				{
-					n = as(s, Node);
-					k = n.numChildren;
-					p = top + k;
-					
-					c = n.child;
-					while (c != null)
+					return top > 0;
+				},
+				next: function()
+				{
+					var s = a[--top];
+					if (s.isNode())
 					{
-						a[p++] = c;
-						c = c.mSibling;
+						n = as(s, Node);
+						k = n.numChildren;
+						p = top + k;
+						
+						c = n.child;
+						while (c != null)
+						{
+							a[p++] = c;
+							c = c.mSibling;
+						}
+						
+						for (i in 0...k)
+						{
+							a[top++] = a[--p];
+						}
 					}
 					
-					for (i in 0...k)
-					{
-						a[top++] = a[--p];
-						a[p] = null;
-					}
+					return as(s.arbiter, SpriteBase);
 				}
-				return as(s.arbiter, SpriteBase);
+			}
+		}
+		else
+		{
+			var a = [];
+			var top = 0, n:Node, s:Spatial, k:Int, p:Int, c:Spatial;
+			
+			n = root.mNode;
+			k = n.numChildren;
+			p = top + k;
+			c = n.child;
+			while (c != null)
+			{
+				a[top++] = c;
+				c = c.mSibling;
+			}
+			
+			return
+			{
+				hasNext: function()
+				{
+					return top > 0;
+				},
+				next: function()
+				{
+					var s = a[--top];
+					if (s.isNode())
+					{
+						n = as(s, Node);
+						k = n.numChildren;
+						p = top + k;
+						
+						c = n.child;
+						while (c != null)
+						{
+							a[p++] = c;
+							c = c.mSibling;
+						}
+						
+						for (i in 0...k)
+						{
+							a[top++] = a[--p];
+						}
+					}
+					
+					return as(s.arbiter, SpriteBase);
+				}
 			}
 		}
 	}
@@ -175,39 +197,30 @@ class SpriteTools
 	@:access(de.polygonal.zz.scene.Spatial)
 	public static function tick(root:SpriteGroup, dt:Float)
 	{
-		var s:Spatial = root.mNode, n:Node;
-		
-		var a = _spatialStack;
-		a.clear();
-		a.pushBack(s);
-		var top = 1;
-		while (top != 0)
+		var stack = _spatialStack, s, n, c;
+		stack.clear();
+		stack.push(root.mNode);
+		while (stack.size > 0)
 		{
-			s = a.popBack();
-			top--;
+			s = stack.pop();
 			
 			if (s.arbiter == null) continue;
 			
 			if (s.controllers != null && s.controllersEnabled)
 				s.updateControllers(dt);
 			
-			if (s.isNode())
+			//SKIP_CHILDREN: SpriteText glyph nodes have no arbiter so don't bother iterating over them
+			if (s.mFlags & (Spatial.IS_NODE | Spatial.SKIP_CHILDREN) == Spatial.IS_NODE)
 			{
 				n = as(s, Node);
-				
-				top += n.numChildren;
-				if (top > a.capacity) a.reserve(top);
-				
-				var c = n.child;
+				c = n.child;
 				while (c != null)
 				{
-					a.unsafePushBack(c);
+					stack.push(c);
 					c = c.mSibling;
 				}
 			}
 		}
-		
-		a.getData().nullify(top);
 	}
 	
 	/**
@@ -215,7 +228,7 @@ class SpriteTools
 	**/
 	public static function freeSubtree(sprite:SpriteBase, includeCaller = false)
 	{
-		if (sprite.isGroup() && !Std.is(sprite, SpriteText))
+		if (sprite.type == SpriteGroup.TYPE)
 		{
 			var c = as(sprite, SpriteGroup).mNode.child, hook;
 			while (c != null)
@@ -229,26 +242,24 @@ class SpriteTools
 		if (includeCaller) sprite.free();
 	}
 	
+	/**
+		Returns true if `sprite` is an ancestor of `target`.
+	**/
 	public static function isAncestor(sprite:SpriteBase, target:SpriteBase):Bool
 	{
-		var result = false;
 		var p = sprite.parent;
 		while (p != null)
 		{
 			if (p == target)
-			{
-				result = true;
-				break;
-			}
+				return true;
 			p = p.parent;
 		}
-		
-		return result;
+		return false;
 	}
 	
 	public static function updateWorldTransform(sprite:SpriteBase, propagateToChildren:Bool = false, updateBounds:Bool = false)
 	{
-		var a = _aSpatial, p = sprite.sgn, s, c = 0;
+		var stack = _spatialStack, p = sprite.sgn, s;
 		while (p != null)
 		{
 			if (p.arbiter != null)
@@ -258,15 +269,12 @@ class SpriteTools
 					s.updateLocalTransform();
 			}
 			
-			a[c++] = p;
+			stack.push(p);
 			p = p.parent;
 		}
 		
-		while (--c > -1)
-		{
-			a[c].updateWorldData(false, false);
-			a[c] = null;
-		}
+		while (stack.size > 0)
+			stack.pop().updateWorldData(false, false);
 		
 		if (sprite.isGroup())
 		{
