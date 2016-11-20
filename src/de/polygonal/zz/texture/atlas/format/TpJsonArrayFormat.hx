e7e399a179738f529989362c35ab2d2941000a79
Michael Baczynski <michaelbaczynski@gmail.com>
2016-11-20 14:53:37 +0100

    Refactor tp json array format to work with closure compiler

--- src/de/polygonal/zz/texture/atlas/format/TpJsonArrayFormat.hx
+++ src/de/polygonal/zz/texture/atlas/format/TpJsonArrayFormat.hx
@@ -18,62 +18,96 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 */
 package de.polygonal.zz.texture.atlas.format;
 
-import de.polygonal.core.math.Coord2.Coord2i;
-import de.polygonal.core.math.Recti;
-import de.polygonal.zz.data.Size.Sizei;
+import de.polygonal.core.util.Assert.assert;
+import de.polygonal.zz.controller.SpriteSheetController.SheetAnimation;
 import de.polygonal.zz.texture.atlas.TextureAtlasFormat;
 import haxe.Json;
 
-private typedef SizeDef = { w:Int, h:Int };
-private typedef RectDef = { > SizeDef, x:Int, y:Int };
-private typedef FrameDef =
-{
-	filename:String,
-	frame:RectDef,
-	rotated:Dynamic,
-	trimmed:Bool,
-	spriteSourceSize:RectDef,
-	sourceSize:SizeDef
-}
-private typedef MetaDef = { size:SizeDef, scale:Float }
+using Reflect;
 
 /**
 	Creates a texture atlas from a TexturePacker JSON-Array file (https://www.codeandweb.com/texturepacker).
 **/
 class TpJsonArrayFormat implements TextureAtlasFormat
 {
-	var mStr:String;
-	
-	public function new(str:String)
+	public static function getSpriteSheetAnims(jsonString:String, holdTime:Float = 1 / 60):Array<SheetAnimation>
 	{
-		mStr = str;
+		var output = [];
+		
+		var o:Dynamic = Json.parse(jsonString);
+		var frames:Array<{filename:String}> = o.field("frames");
+		var fileNames = Lambda.array(Lambda.map(frames, function(e) return e.filename));
+		var ereg = ~/(.*?)[\/_](\d{4})$/;
+		var currentAni:SheetAnimation = null, currentName = null, index = 0, frame = -1;
+		for (i in fileNames)
+		{
+			var matched = ereg.match(i);
+			
+			if (!matched)
+			{
+				index++;
+				continue;
+			}
+			
+			var name = ereg.matched(1);
+			if (currentName != name)
+			{
+				if (currentAni != null)
+					output.push(currentAni);
+				currentName = name;
+				frame = Std.parseInt(ereg.matched(2));
+				currentAni = {name: name, loop: false, frames: []};
+			}
+			else
+			{
+				frame++;
+				assert(frame == Std.parseInt(ereg.matched(2)));
+			}
+			
+			currentAni.frames.push({value: i, holdTime: holdTime});
+			index++;
+		}
+		
+		if (currentAni != null) output.push(currentAni);
+		return output;
+	}
+	
+	var mJsonString:String;
+	
+	public function new(jsonString:String)
+	{
+		mJsonString = jsonString;
 	}
 	
 	public function getAtlas():TextureAtlasDef 
 	{
-		var data:TextureAtlasDef = {size: new Sizei(), scale: 1., frames: []};
+		var data = new TextureAtlasDef();
 		
-		var o:Dynamic = Json.parse(mStr);
-		var meta:MetaDef = Reflect.field(o, "meta");
+		var o:Dynamic = Json.parse(mJsonString);
 		
-		data.size.x = meta.size.w;
-		data.size.y = meta.size.h;
-		data.scale = meta.scale;
-		
-		inline function toRect(o:RectDef):Recti return new Recti(o.x, o.y, o.w, o.h);
+		var meta:Dynamic = Reflect.field(o, "meta");
+		data.size.x = meta.field("size").field("w");
+		data.size.y = meta.field("size").field("h");
+		data.scale = cast meta.field("scale");
 		
 		var index = 0;
-		var frames:Array<FrameDef> = Reflect.field(o, "frames");
+		var frames:Array<Dynamic> = o.field("frames");
 		for (i in frames)
 		{
-			data.frames.push({
-					index: index++,
-					name: i.filename,
-					cropRect: toRect(i.frame),
-					trimFlag: i.trimmed,
-					sourceSize: new Sizei(i.sourceSize.w, i.sourceSize.h),
-					trimOffset: new Coord2i(i.spriteSourceSize.x, i.spriteSourceSize.y)
-				});
+			var f = new TextureAtlasFrameDef();
+			data.frames.push(f);
+			
+			f.index = index++;
+			f.name = i.field("filename");
+			f.cropRect.x = i.field("frame").field("x");
+			f.cropRect.y = i.field("frame").field("y");
+			f.cropRect.w = i.field("frame").field("w");
+			f.cropRect.h = i.field("frame").field("h");
+			f.trimFlag = i.field("trimmed");
+			f.sourceSize.x = i.field("sourceSize").field("w");
+			f.sourceSize.y = i.field("sourceSize").field("h");
+			f.trimOffset.x = i.field("spriteSourceSize").field("x");
+			f.trimOffset.y = i.field("spriteSourceSize").field("y");
 		}
 		
 		return data;
